7f08ef5b838f48fe867c7288375e4cf2f56648b1
Irwin Ramirez <Irwinramirez23@gmail.com>
2022-06-25 02:15:33 -0400

    commit tree 29b1a657bec42e73ea0204afac4b0c8cbde9907d

--- main.js
+++ main.js
@@ -2,7 +2,39 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const hideError = document.querySelector("#modal")
+const allHearts = document.querySelectorAll(".like-glyph")
+
+  hideError.classList.add("hidden")
+
+function globalLiker () {
+ document.addEventListener("click", (event) => {
+   if(event.target.classList[0] === "like-glyph"){
+     mimicServerCall()
+     .then((resp) => {
+      const activated = event.target.classList.contains("activated-heart")
+      if (activated) {
+        event.target.classList.remove("activated-heart")
+        event.target.innerHTML = EMPTY_HEART;
+      }else 
+        event.target.classList.add("activated-heart")
+        event.target.innerHTML = FULL_HEART;
+      
+
+      activated;
+     })
+     
+     .catch((error) => {
+      hideError.classList.remove("modal")
+      setTimeout(() => {
+        hideError.classList.add("modal")},3000)
+    })
+   }
+ })
+}
+
+globalLiker()
+
 
 
 
